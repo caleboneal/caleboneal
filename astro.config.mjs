984bbd032a154a7d5af9724ef2012854a4aeb581
Caleb O'Neal <caleboneal07@gmail.com>
2026-07-15 12:32:18 -0400

    Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  site: 'https://caleboneal.dev',
+  adapter: cloudflare(),
+  integrations: [mdx()]
 });
