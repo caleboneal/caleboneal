85568dcb87b70e2f4a9a274f08e63f628421f308
Caleb O'Neal <caleboneal07@gmail.com>
2026-07-15 13:16:47 -0400

    Starter site

---
+++ src/data/site.ts
@@ -0,0 +1,12 @@
+export const site = {
+	name: "Caleb O'Neal",
+	monogram: 'CO',
+	tagline: 'Software Engineer',
+	description: "Personal site and writing by Caleb O'Neal.",
+	url: 'https://caleboneal.dev',
+	links: {
+		x: '#',
+		linkedin: '#',
+		resume: '#',
+	},
+} as const;
