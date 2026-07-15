ca2034a48d954e4eb6532685fc829ec79316be7e
Caleb O'Neal <caleboneal07@gmail.com>
2026-07-15 14:16:28 -0400

    Add social links

--- src/data/site.ts
+++ src/data/site.ts
@@ -5,9 +5,9 @@ export const site = {
 	description: "Personal site and writing by Caleb O'Neal.",
 	url: 'https://caleboneal.dev',
 	links: {
-		x: '#',
-		linkedin: '#',
-		github: '#',
-		resume: '#',
+		x: 'https://x.com/CalebONeal07',
+		linkedin: 'https://linkedin.com/in/caleboneal2007',
+		github: 'https://github.com/caleboneal',
+		resume: '/resume.pdf',
 	},
 } as const;
