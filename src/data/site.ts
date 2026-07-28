9f97300a36ff4c26407e0463d9143e4cab081f18
Caleb O'Neal <caleboneal07@gmail.com>
2026-07-28 06:54:43 -0400

    Remove unused code, make icons data driven, and clean up code duplication

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,5 @@
 export const site = {
 	name: "Caleb O'Neal",
-	monogram: 'CO',
-	tagline: 'Software Engineer',
 	description: "Personal site and writing by Caleb O'Neal.",
 	url: 'https://caleboneal.dev',
 	links: {
