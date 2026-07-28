61d68f04eb5512a5fcdc4d66d8409fa6c37650b3
Caleb O'Neal <caleboneal07@gmail.com>
2026-07-28 14:23:25 -0400

    Update site description and add cloudflareinsights preconnect

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,6 +1,6 @@
 export const site = {
 	name: "Caleb O'Neal",
-	description: "Personal site and writing by Caleb O'Neal.",
+	description: "I'm a Computer Engineering student at the University of Virgina. I do systems programming and write about it here sometimes.",
 	url: 'https://caleboneal.dev',
 	ogImage: '/og-default.png',
 	links: {
